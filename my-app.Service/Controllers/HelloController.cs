52924890e58ebccf023011f9dd4f4785a82bfd22
Liran Mizrahi <73521370+LiranMizrahi@users.noreply.github.com>
2024-06-03 12:39:56 +0000

    new image tag

--- my-app.Service/Controllers/HelloController.cs
+++ my-app.Service/Controllers/HelloController.cs
@@ -6,10 +6,25 @@ namespace my_app.Service.Controllers
     [Route("[controller]")]
     public class HelloController : ControllerBase
     {
+        private static readonly HttpClient client = new HttpClient();
+        
         [HttpGet]
         [Route("hello")]
-        public IActionResult GetHello()
+        public async Task<IActionResult> GetHelloAsync()
         {
+            try
+        {
+            HttpResponseMessage response = await client.GetAsync("http://localhost:8080/user17.user17-argocd/hello/test");
+            response.EnsureSuccessStatusCode(); // Throw an exception if the response is not successful
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+           
             var value = System.Environment.GetEnvironmentVariable("ENDPOINT_URL");
             Console.WriteLine(value);
             return Ok(value);
